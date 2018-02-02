64b57d7eff60ac758f056f6456ad07e3b38eaa25
Taylor Buckner <tbuckner@theappraisallane.com>
2018-02-02 11:14:33 -0600

    Added chalk coliring for fun. Added some feature requestsfor indivdualized reports. Began structure for update data rather than append. TODO: Add support for users outside of our Organization

--- config/config.js
+++ config/config.js
@@ -8,15 +8,35 @@ module.exports = {
     {
       type: 'confirm',
       name: 'initial',
-      message: 'Is this your first time using the tool? (Default: False)',
+      message: 'Is this your first time using the tool? \n(Default: No)',
       default: false,
+    },
+    {
+      type: 'list',
+      name: 'operation',
+      message: 'What opearation are you looking to run? \n(Default: Populate Blank Sheet(s))',
+      choices: ['Populate Blank Sheet(s)', 'Update Existing Sheet(s)'],
+      default: 'Populate Blank Sheets(s)',
+      when: (answers) => {
+        return answers.initial === false
+      }
+    },
+    {
+      type: 'list',
+      name: 'reportType',
+      message: 'What report are you looking to run? \n(Default: Both)',
+      choices: ['Demo Sheet', 'PlanITPoker', 'Both'],
+      default: 'Both',
+      when: (answers) => {
+        return answers.initial === false
+      }
     },
     {
       type: 'list',
       name: 'project',
       message: 'Select the Jira Project',
       default: 'Retail',
-      choices: ['RT', 'CORE', 'TBN', 'User Defined'],
+      choices: ['RT', 'CORE', 'TBN', 'CORE & TBN', 'User Defined'],
       when: (answers) => {
         return answers.initial === false
       }
@@ -58,7 +78,7 @@ module.exports = {
     {
       type: 'confirm',
       name: 'tabConfirmation',
-      message: 'Would you like to use the default sheet names? (\'Sheet1\', \'Sheet2\')',
+      message: 'Does your spreadsheet use default sheet names? \n(\'Sheet1\' And/Or \'Sheet2\')',
       default: true,
       when: (answers) => {
         return answers.initial === false
@@ -67,19 +87,19 @@ module.exports = {
     {
       type: 'input',
       name: 'sheet1',
-      message: 'Enter the name of Sheet/Tab 1:',
+      message: 'Enter the name of Sheet/Tab Demo Sheet:',
       default: null,
       when: (answers) => {
-        return answers.tabConfirmation === false
+        return answers.tabConfirmation === false && (answers.reportType === 'Both' || answers.reportType === 'Demo Sheet')
       }
     },
     {
       type: 'input',
       name: 'sheet2',
-      message: 'Enter the name of Sheet/Tab :',
+      message: 'Enter the name of PlanITPoker Sheet/Tab:',
       default: null,
       when: (answers) => {
-        return answers.tabConfirmation === false
+        return answers.tabConfirmation === false && (answers.reportType === 'Both' || answers.reportType === 'PlanITPoker')
       }
     },
   ],
